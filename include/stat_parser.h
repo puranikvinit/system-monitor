965cf138c3cdc65f546e483a3509fc6704591417
puranikvinit <kvp933.vinit@gmail.com>
2024-05-06 20:09:38 +0530

    define header files for the project

---
+++ include/stat_parser.h
@@ -0,0 +1,64 @@
+#ifndef STAT_STRUCT_H
+#define STAT_STRUCT_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+/*! \brief Contains data aquired when reading the cpuN data from `/proc/stat`*/
+typedef struct {
+    // Units in USER_HZ
+
+    // Total time spent by user mode processes
+    unsigned long long int user;
+
+    // Total time spent by niced user mode processes
+    unsigned long long int nice;
+
+    // Total time spent by system mode processes
+    unsigned long long int system;
+
+    // Total time spent by idle processes
+    unsigned long long int idle;
+
+    // Total time spent waiting for I/O to complete
+    unsigned long long int io_wait;
+
+    // Total time spent servicing hardware interrupts
+    unsigned long long int irq;
+
+    // Total time spent servicing software interrupts
+    unsigned long long int soft_irq;
+
+    // Total time spent in other Operating Systems when running in a virtualized environment
+    unsigned long long int steal;
+
+    // Total time spent running a virtual CPU for guest Operating Systems
+    unsigned long long int guest;
+
+    // Total time spent running a niced guest operating system
+    unsigned long long int guest_nice;
+} cpu_struct_t;
+
+
+/*! \brief Contains data from a whole reading of `/proc/stat` file*/
+typedef struct {
+    // time_t sampleTimeMS;
+    size_t cpu_num;
+    cpu_struct_t *cpu;
+} stat_struct_t;
+
+
+// Member APIs
+
+/*! \brief Scans the contents of `/proc/stat` to determine number of CPUs*/
+bool scan_proc_stat(size_t *cpu_num);
+
+/*! \brief Reads the most important contents of the `/proc/stat` file*/
+bool read_proc_stat(stat_struct_t *stat);
+
+/*! \brief Prints the contents of `stat_struct_t` to the terminal, for debugging*/
+void print_proc_stat(stat_struct_t *stat);
+
+#endif
---
+++ include/usage_calculator.h
@@ -0,0 +1,16 @@
+#ifndef CPU_ANALYSER_H
+#define CPU_ANALYSER_H
+
+#include "stat_parser.h"
+
+// Member APIs
+
+/*! \brief calculates CPU usage
+ *  \returns usage in percentage */
+float calculate_cpu_use_per_c(cpu_struct_t prev_cpu, cpu_struct_t cur_cpu);
+
+/*! \brief calculates CPU usage of all CPUs in `/proc/stat`; puts it on the logQueue
+    \returns false if the cpuN changes between two consecutive reads */
+bool calculate_all_cpus(stat_struct_t *prev_stat, stat_struct_t *cur_stat, size_t cpu_num, float *cpu_per_cs);
+
+#endif
