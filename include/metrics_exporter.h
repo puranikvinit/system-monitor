3bad985eec15e7ab9f57e65740ad4cd598039077
puranikvinit <kvp933.vinit@gmail.com>
2024-05-23 18:15:02 +0530

    write into accumulator file, and serve requests from that file rather than directly serving from variable

--- include/metrics_exporter.h
+++ include/metrics_exporter.h
@@ -1,10 +1,15 @@
 #ifndef METRICS_EXPORTER_H
 #define METRICS_EXPORTER_H
 
+#include "accumulator_queue.h"
+
+#define ACCUMULATED_METRICS_FILE "./accumulated_metrics.txt"
 
 // Member APIs
 
-void record_metrics(float *cpu_percs, size_t cpu_num, char* accumulated_metrics);
+void record_metrics(float *cpu_percs, size_t cpu_num, accumulator_queue_t *accumulator_queue);
+
+void write_metrics(accumulator_queue_t *accumulator_queue);
 
 /*! \brief exports usage metrics to the opened socket by the socket_manager */
 void export_metrics(int new_socket, char* accumulated_metrics);
--- source/metrics_exporter.c
+++ source/metrics_exporter.c
@@ -1,24 +1,52 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 
 #include "metrics_exporter.h"
+#include "accumulator_queue.h"
 
-void record_metrics(float *cpu_percs, size_t cpu_num, char* accumulated_metrics) {
+void record_metrics(float *cpu_percs, size_t cpu_num, accumulator_queue_t *accumulator_queue) {
     char usage_metrics[100000] = "";
 
     for (size_t i = 0; i < cpu_num; i++) {
-        printf("CPU %zu Usage: %.2f%%\n", i, cpu_percs[i]);
+        // printf("CPU %zu Usage: %.2f%%\n", i, cpu_percs[i]);
         char current_cpu_usage[256];
-        sprintf(current_cpu_usage, "CPU %zu Usage: %.2f%%\n", i, cpu_percs[i]);
+        sprintf(current_cpu_usage, "CPU %zu Usage: %.2f%% ", i, cpu_percs[i]);
         strcat(usage_metrics, current_cpu_usage);
     }
-    strcat(accumulated_metrics, usage_metrics);
+    strcat(usage_metrics, "\n");
+
+    // enqueue(&accumulator_queue, usage_metrics);
+    if (!isFull(accumulator_queue)) {
+        enqueue(accumulator_queue, usage_metrics);
+    } else {
+        printf("ERROR: Accumulator queue is full!\n");
+    }
 }
 
-void export_metrics(int new_socket, char* usage_metrics) {
-    char* http_response[100000];
-    sprintf(http_response, "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n" "Content-Length: %d \r\n" "\r\n%s", strlen(usage_metrics), usage_metrics);
+void write_metrics(accumulator_queue_t *accumulator_queue) {
+    // strcat(accumulated_metrics, usage_metrics);
+    FILE *accumulated_metrics = NULL;
+    if ((accumulated_metrics = fopen(ACCUMULATED_METRICS_FILE, "a")) == NULL) {
+        accumulated_metrics = fopen(ACCUMULATED_METRICS_FILE, "w");
+        // fprintf(accumulated_metrics, "%s", usage_metrics);
+        while (!isEmpty(accumulator_queue)) {
+            fprintf(accumulated_metrics, "%s", dequeue(accumulator_queue));
+        }
+        fclose(accumulated_metrics);
+    } else {
+        // fprintf(accumulated_metrics, "%s", usage_metrics);
+        while (!isEmpty(accumulator_queue)) {
+            fprintf(accumulated_metrics, "%s", dequeue(accumulator_queue));
+        }
+        fclose(accumulated_metrics);
+    }
+}
+
+void export_metrics(int new_socket, char* accumulator_metrics) {
+    char http_response[100000];
+    sprintf(http_response, "HTTP/1.1 200 OK\r\n" "Content-Type: text/plain\r\n" "Content-Length: %ld \r\n" "\r\n%s", strlen(accumulator_metrics), accumulator_metrics);
 
     send(new_socket, http_response, strlen(http_response), 0);
 }
